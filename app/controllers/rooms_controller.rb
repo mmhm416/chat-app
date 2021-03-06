7a0cf0a76c1c332723913319a2423216740bba8d
mmhm416 <tma.hshmt@gmail.com>
2021-03-06 18:58:21 +0900

    プルダウンの実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -4,4 +4,19 @@ class RoomsController < ApplicationController
   @rooms = Room.new
   end
 
+  def create
+    @room = Room.new(room_params)
+    if @room.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def room_params
+    params.require(:room).permit(:name, user_ids: [])
+  end
+
 end
